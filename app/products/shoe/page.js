bfb3ab74fe51775fa390e0f6d0da2f8aa322efae
Manuel Baumgartner <manu.baumi@outlook.com>
2023-05-25 20:15:52 +0200

    Add button

--- app/products/shoe/page.js
+++ app/products/shoe/page.js
@@ -12,7 +12,6 @@ export const metadata = {
 export default function ShoePage() {
   const [quantity, setQuantity] = useState(1);
   const [counter, setCounter] = useState(0);
-  const [isInCart, setIsInCart] = useState(false);
 
   const handleQuantityChange = (event) => {
     const newQuantity = parseInt(event.target.value);
@@ -21,57 +20,47 @@ export default function ShoePage() {
     }
   };
   const handleAddToCart = () => {
-    if (quantity <= 0) {
-      alert('Please enter a valid quantity.');
-      return;
-    }
+    if (quantity <= 0) return;
   };
-  setIsInCart(true);
-  alert('Product added to cart!');
-
-  const handleRemoveFromCart = () => {
-    setIsInCart(false);
-    alert('Product removed from cart!');
-  };
-
-  return (
-    <>
-      <div
-        style={{
-          zIndex: '-1',
-          position: 'fixed',
-          width: '100vw',
-          height: '100vh',
-        }}
-      >
-        <Image src="/Mountainview.jpg" alt="mountainview" layout="fill" />
-      </div>
-      <main>
-        <h1 className={styles.h1}> Shoes</h1>
-        <div className={styles.picture}>
-          <img
-            src="/shoe.png"
-            data-test-id="product-image"
-            alt="Product Shoe"
-            height="500"
-          />
-          <span data-test-id="product-price">99.99</span>
-
-          <input
-            type="number"
-            data-test-id="product-quantity"
-            value="1"
-            min="1"
-          />
-          <p>counter: {counter}</p>
-          <button data-test-id="product-add-to-cart">Add to Cart</button>
-          <button onClick={handleRemoveFromCart}>Remove from Cart</button>
-        </div>
-        <div className={styles.h2}>
-          <h2> Key Facts</h2>
-          <p> Here should be a Text </p>
-        </div>
-      </main>
-    </>
-  );
 }
+
+return (
+  <>
+    <div
+      style={{
+        zIndex: '-1',
+        position: 'fixed',
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <Image src="/Mountainview.jpg" alt="mountainview" layout="fill" />
+    </div>
+    <main>
+      <h1 className={styles.h1}> Shoes</h1>
+      <div className={styles.picture}>
+        <img
+          src="/shoe.png"
+          data-test-id="product-image"
+          alt="Product Shoe"
+          height="500"
+        />
+        <span data-test-id="product-price">99.99</span>
+
+        <input
+          type="number"
+          data-test-id="product-quantity"
+          value="1"
+          min="1"
+        />
+        <p>counter: {counter}</p>
+        <button data-test-id="product-add-to-cart">Add to Cart</button>
+        <button onClick={handleRemoveFromCart}>Remove from Cart</button>
+      </div>
+      <div className={styles.h2}>
+        <h2> Key Facts</h2>
+        <p> Here should be a Text </p>
+      </div>
+    </main>
+  </>
+);
